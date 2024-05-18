7b47ee88f4a3664b24fb7859bfa88b10de4ac81c
yassin <ahmedoyassin02@gmail.com>
2024-05-18 17:09:26 +0300

    Adding NVIC Driver

---
+++ NVIC/NVIC Driver/MNVIC_cfg.h
@@ -0,0 +1,20 @@
+/*
+ * NVIC_cfg.h
+ *
+ *  Created on: Jul 29, 2023
+ *      Author: Ahmed
+ */
+
+#ifndef MNVIC_CFG_H_
+#define MNVIC_CFG_H_
+
+#define GGGG 0x05FA0300 // 16 NESTED
+#define GGGS 0x05FA0400
+#define GGSS 0x05FA0500 // SEMI-NESTED
+#define GSSS 0x05FA0600
+#define SSSS 0x05FA0700 //  NON-NESTED
+
+#define PRIGROUP GGSS
+
+
+#endif /* MNVIC_CFG_H_ */
